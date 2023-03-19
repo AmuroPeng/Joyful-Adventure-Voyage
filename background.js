de2b8fa966ec5c4edb482d3f05ee362df8ee45f1
AmuroPeng <tiggerpeng1997@gmail.com>
2023-03-19 21:41:33 +0800

    example done

---
+++ background.js
@@ -0,0 +1,19 @@
+$(function(){
+    // 从存储中获取计数器值
+    chrome.storage.local.get('count', function(data) {
+        var count = data.count || 0;
+        $('#input').val(count);
+    });
+
+    // 在存储中保存计数器值
+    $('#btn').click(function() {
+        chrome.storage.local.get('count', function(data) {
+            var count = data.count || 0;
+            chrome.storage.local.set({count: count + 1}, function() {
+                $('#input').val(count + 1);
+            });
+        });
+    });
+});
+
+
